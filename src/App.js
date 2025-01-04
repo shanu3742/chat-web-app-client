0735343c4d309541daaeb6db71774249743cc2ac
kumar <krksingh.99@gmail.com>
2025-01-05 01:47:13 +0530

    animation logic updated

--- src/App.js
+++ src/App.js
@@ -1,41 +1,49 @@
 import ChatRoutes from './ChartRoutes';
 import UserContext from './context/userContext';
-import './App.scss'
+import './App.scss';
 import { Slide } from "@mui/material";
 import { ToastContainer } from 'react-toastify';
 import { useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
-
-
 function App() {
   const location = useLocation();
-  const [isSlide,setSlide] = useState(false)
+  const [currentPath, setCurrentPath] = useState(location.pathname);
+  const [isAnimating, setIsAnimating] = useState(false);
+
 
   useEffect(() => {
-    setSlide((p) => !p)
-  },[location.pathname])
+    if (location.pathname !== currentPath) {
+      setIsAnimating(true);
+      const timeout = setTimeout(() => {
+        setCurrentPath(location.pathname);
+        setIsAnimating(false); 
+      }, 1);
 
-  useEffect(() => {
-    if(!isSlide){
-     setTimeout(() => {
-      setSlide(true)
-     },500)
+      return () => clearTimeout(timeout); 
     }
-  },[isSlide])
+  }, [location.pathname, currentPath]);
 
   return (
     <UserContext>
-    <div className="App">
-      <ToastContainer />
-      <Slide direction={isSlide?"left":'right'} in={isSlide} >
-        <div>
-          <ChatRoutes />
-        </div>
-      </Slide>
-    </div>
+      <div className="App">
+        <ToastContainer />
+        {isAnimating && (
+          <Slide direction={'left'} in={!isAnimating} timeout={300}>
+            <div className="outgoing-page">
+              <ChatRoutes key={currentPath} />
+            </div>
+          </Slide>
+        )}
+        {!isAnimating && (
+          <Slide direction={'left'} in={!isAnimating} timeout={300}>
+            <div className="incoming-page">
+              <ChatRoutes key={currentPath} />
+            </div>
+          </Slide>
+        )}
+      </div>
     </UserContext>
-   
   );
 }
 
